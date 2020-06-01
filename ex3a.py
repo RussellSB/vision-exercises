38e72c401d7484c2fd82f76ba188fb588a5abcc1
RussellSB <r.sammutbonnici@gmail.com>
2020-06-01 09:34:28 +0200

    Find union attempt

--- ex3a.py
+++ ex3a.py
@@ -12,41 +12,70 @@ def has_neighbour(im_labels, y, x):
     except (IndexError, ValueError):
         return False
 
+def find_union(mini, maxi, compenents):
+    min_set_i = 0
+    max_set_i = 0
+
+    for i in range (0, len(compenents)):
+        set = compenents[i]
+        if mini in set:
+            min_set_i = i
+
+    for i in range (0, len(compenents)):
+        set = compenents[i]
+        if maxi in set:
+            max_set_i = i
+
+
+    if max_set_i != min_set_i:
+        print(min_set_i, max_set_i)
+        compenents[min_set_i] = compenents[min_set_i].union(compenents[max_set_i])
+        del compenents[max_set_i]
+    return compenents
+
 def four_connected_components_labelling(im):
     h, w = im.shape[0], im.shape[1]
 
-    # Dictionary of labels of pixels
     im_labels = np.zeros(im.shape, dtype='uint8')
     new_label_cnt = 1  # Keeps track of the newest component group
+    components = []
+
+    # 1st pass
     for y in range(0, h):
         for x in range(0, w):
             hasTop = has_neighbour(im_labels, y - 1, x)
-            hasBottom = has_neighbour(im_labels, y + 1, x)
             hasLeft = has_neighbour(im_labels, y, x - 1)
-            hasRight = has_neighbour(im_labels, y, x + 1)
 
             if(x == 126 and y == 76):
                 print("debug")
 
             if (im[y, x] != 0):
-                if(not hasTop and not hasBottom and not hasLeft and not hasRight):
+                if(not hasTop and not hasLeft):
                     im_labels[y, x] = new_label_cnt
-                    print(new_label_cnt)
+                    components.append({new_label_cnt})
                     new_label_cnt += 1
                 else:
-                    if(hasBottom):
-                        im_labels[y, x] = im_labels[y + 1, x]
-                    if (hasRight):
-                        im_labels[y, x] = im_labels[y, x + 1]
-                    if (hasTop):
-                        im_labels[y, x] = im_labels[y - 1, x]
-                    if (hasLeft):
+                    if (hasLeft and not hasTop):
                         im_labels[y, x] = im_labels[y, x - 1]
+                    elif (hasTop and not hasLeft):
+                        im_labels[y, x] = im_labels[y - 1, x]
+                    elif (hasTop and hasLeft):
+                        top = im_labels[y - 1, x]
+                        left = im_labels[y, x - 1]
+                        if(top != left):
+                            (mini, maxi) = (left,top) if left < top else (top, left)
+                            im_labels[y, x] = mini
+                            components = find_union(mini, maxi, components)
 
-    # cv2.imshow('4-Connectivity', im_labels)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
-    print('debug')
+
+    print('=============')
+
+    # 2nd pass
+    # TODO
+
+    cv2.imshow('4-Connectivity', im_labels)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 
 def main():
